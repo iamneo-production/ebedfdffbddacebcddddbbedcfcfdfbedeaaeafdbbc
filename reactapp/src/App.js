bad28d0e79eaea5dfed40c01c2898718a52b81fe
Iamneo <techadmin@iamneo.ai>
2023-08-08 07:07:55 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -45,11 +45,12 @@ function App() {
     300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} */}
                   {banner && <Banner score={attempt} />}
                 {card && <div> {questions.map((q, index) => (
-    <Card key={index} question={q.question} correctAnswerMarkUpda
-    te={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsn Attempt} />
+                    <Card key={index} question={q.question} correctAnswerMarkUpda
+    te={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} 
+     setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />
     ))} </div>}
-    {result && <Button onClick={handleResult} style={"bg-orange-300 px-
-    10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
+    {result && <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"}
+    >Show Results</Button>}
     {start && <Button onClick={ handleStart} style={"bg-orange-300 px10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} </div>
     </div>
     );
