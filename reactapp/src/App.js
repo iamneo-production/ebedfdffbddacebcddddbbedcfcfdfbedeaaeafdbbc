8d025aadd2e5fd6ee8d514c397d5880a2855af2b
Iamneo <techadmin@iamneo.ai>
2023-08-08 07:02:54 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -23,21 +23,28 @@ function App() {
     const opt = {
     option1 : "Blue",option2 : "Red",option3 : "Yellow",option4 : "Green"
     }
-    // Array of questions const questions = [	{ question: "What color are the leaves?", answer: "Gr een" },	{ question: "What color is the sky?", answer: "Blue" },	{ questi on: "What color is a banana?", answer: "Yellow" },	{ question: "What color is a strawberry?", answer: "Red" },	{ question: "What color is a watermelon? ", answer: "Green" } ];
-    // Event handler to show the result of the Quiz const handleResult = () => { setStart(true) setBanner(true)
-    setCard(false) setResult(false)
+    // Array of questions 
+    const questions = [	{ question: "What color are the leaves?", answer: "Green" },
+    	{ question: "What color is the sky?", answer: "Blue" }, 	{ questi on: "What color is a banana?", answer: "Yellow" },	
+        { question: "What color is a strawberry?", answer: "Red" },	{ question: "What color is a watermelon? ", answer: "Green" } ];
+    // Event handler to show the result of the Quiz
+    const handleResult = () => { 
+        setStart(true) 
+        setBanner(true)
+        setCard(false) 
+        setResult(false)
     }
     return (
-    <div className="flex justify-center mt-4 ">
-    <div>
-    <div className="flex items-center justify-center space-x-4 mb-12">
-    <h1 className="text-3xl">Quizz App</h1>
-    <i className="">learn react</i>
-    </div>
-    {/* {start && <Button onClick={ handleStart} style={"bg-orange-
+      <div className="flex justify-center mt-4 ">
+        <div>
+            <div className="flex items-center justify-center space-x-4 mb-12">
+              <h1 className="text-3xl">Quizz App</h1>
+              <i className="">learn react</i>
+            </div>
+                {/* {start && <Button onClick={ handleStart} style={"bg-orange-
     300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>	} */}
-    {banner && <Banner score={attempt} />}
-    {card && <div> {questions.map((q, index) => (
+                  {banner && <Banner score={attempt} />}
+                {card && <div> {questions.map((q, index) => (
     <Card key={index} question={q.question} correctAnswerMarkUpda
     te={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer} setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsn Attempt} />
     ))} </div>}
